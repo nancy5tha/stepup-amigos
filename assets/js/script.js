576d1ebbb83dfc7ba54b867a19792fc8f97ec5bb
nancy5tha <yacysthasky@gmail.com>
2025-09-13 14:59:08 +0545

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,42 @@
 
-  const menubar = document.getElementById("menu-bar");
-  const navbar = document.getElementById("navbar-menu");
-  const icon = menubar.querySelector("i");
+  // const menubar = document.getElementById("menu-bar");
+  // const navbar = document.getElementById("navbar-menu");
+  // const icon = menubar.querySelector("i");
 
-  menubar.addEventListener("click", () => {
-    navbar.classList.toggle("active");
+  // menubar.addEventListener("click", () => {
+  //   navbar.classList.toggle("active");
 
   
-    if (navbar.classList.contains("active")) {
-      icon.classList.remove("fa-bars-staggered");
-      icon.classList.add("fa-xmark");
-    } else {
-      icon.classList.remove("fa-xmark");
-      icon.classList.add("fa-bars-staggered");
+  //   if (navbar.classList.contains("active")) {
+  //     icon.classList.remove("fa-bars-staggered");
+  //     icon.classList.add("fa-xmark");
+  //   } else {
+  //     icon.classList.remove("fa-xmark");
+  //     icon.classList.add("fa-bars-staggered");
+  //   }
+  // });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBar = document.getElementById("menu-bar");
+  const navbarMenu = document.getElementById("navbar-menu");
+  const closeBtn = document.getElementById("menu-close");
+  const dropdownLinks = document.querySelectorAll("#navbar-menu .chevron");
+
+  menuBar.addEventListener("click", () => {
+    navbarMenu.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    navbarMenu.classList.remove("active");
+  });
+
+  document.addEventListener("click", function(e) {
+    if (!navbarMenu.contains(e.target) && !menuBar.contains(e.target)) {
+      navbarMenu.classList.remove("active");
     }
   });
 
+ 
+});
